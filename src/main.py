ec2cb61a256f943e972671522699410fe17aa67e
Kian Kheiri <kingkk8181@gmail.com>
2025-09-30 03:23:33 +0330

    Fix inputs and replay.

--- src/main.py
+++ src/main.py
@@ -6,25 +6,26 @@ class TicTacToe:
     
     def show_board(self):
         print("\n")
-        print(self.board[1] + "|" + self.board[2] + "|" + self.board[3] )
-        print("-----")
-        print(self.board[4] + "|" + self.board[5] + "|" + self.board[6] )
-        print("-----")
-        print(self.board[7] + "|" + self.board[8] + "|" + self.board[9] )
+        print(f" {self.board[1]} | {self.board[2]} | {self.board[3]}")
+        print("---+---+---")
+        print(f" {self.board[4]} | {self.board[5]} | {self.board[6]}")
+        print("---+---+---")
+        print(f" {self.board[7]} | {self.board[8]} | {self.board[9]}")
         print("\n")
-        
+
     def fill_board(self, player, cell):
         self.board[cell] = player
     
     def has_player_won(self, player):
-        return ((self.board[7] == player and self.board[8] == player and self.board[9] == player) or 
-                (self.board[4] == player and self.board[5] == player and self.board[6] == player) or 
-                (self.board[1] == player and self.board[2] == player and self.board[3] == player) or 
-                (self.board[7] == player and self.board[4] == player and self.board[1] == player) or 
-                (self.board[8] == player and self.board[5] == player and self.board[2] == player) or 
-                (self.board[9] == player and self.board[6] == player and self.board[3] == player) or     
-                (self.board[7] == player and self.board[5] == player and self.board[3] == player) or 
-                (self.board[9] == player and self.board[5] == player and self.board[1] == player))
+        b = self.board
+        return ((b[1] == b[2] == b[3] == player) or
+                (b[4] == b[5] == b[6] == player) or
+                (b[7] == b[8] == b[9] == player) or
+                (b[1] == b[4] == b[7] == player) or
+                (b[2] == b[5] == b[8] == player) or
+                (b[3] == b[6] == b[9] == player) or
+                (b[1] == b[5] == b[9] == player) or
+                (b[3] == b[5] == b[7] == player))
     
     def change_turn(self):
         if self.player_turn == "X":
@@ -41,38 +42,38 @@ class TicTacToe:
             self.show_board()
             try:
                 user_input = int(input(f"Player {self.player_turn}, Enter a number between 1-9: "))
+                if user_input not in range(1, 10):
+                    print("Number out of range. Choose 1-9.")
+                    continue
+                if self.board[user_input] != ' ':
+                    print("Cell already filled. Choose another.")
+                    continue
             except ValueError:
-                print("Enter a -Number-!! (between 1-9)")
+                print("Invalid input! Enter a number between 1-9.")
                 continue
             
-            if user_input in range(1,10) and self.board[user_input] == ' ':
-                self.fill_board(self.player_turn, user_input)
+            self.fill_board(self.player_turn, user_input)
                 
-                if self.is_board_fill() and self.has_player_won is False:
-                        self.show_board()
-                        print("Draw!!")
-                        play_again = input("Do you want to play again? (yes for play, anything else for quit.\n)")
-                        if play_again.lower() == "yes":
-                            self.board = [' '] * 10
-                            continue
-                        else:
-                            break
+            if self.is_board_fill():
+                    self.show_board()
+                    print("It's a draw!")
+                    if input("Play again? (\"yes\" to continue): ").lower() == "yes":
+                        self.board = [' '] * 10
+                        continue
+                    else:
+                        break
                     
-                elif self.has_player_won(self.player_turn):
-                        self.show_board()
-                        print(f"{self.player_turn} has won the game!!")
-                        play_again = input("Do you want to play again? (yes for play, anything else for quit.\n)")
-                        if play_again.lower() == "yes":
-                            self.board = [' '] * 10
-                            continue
-                        else:
-                            break
+            elif self.has_player_won(self.player_turn):
+                self.show_board()
+                print(f"{self.player_turn} has won the game!!")
+                if input("Play again? (\"yes\" to continue): ").lower() == "yes":
+                    self.board = [' '] * 10
+                    continue
+                else:
+                    break
                     
-                self.change_turn()
+            self.change_turn()
                 
-            else:
-                print("\nInvalid cell number, Try again!\n( Out of range (1-9) or chose an already filled cell.)")
-
 if __name__ == "__main__":
     game = TicTacToe()
     game.start()
